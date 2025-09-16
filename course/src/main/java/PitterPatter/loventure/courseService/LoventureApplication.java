303b36c506f77957cf60cf88d4396f1752bd352d
YongwanJoo <113014598+YongwanJoo@users.noreply.github.com>
2025-09-16 16:20:00 +0900

    Add files via upload

---
+++ course/src/main/java/PitterPatter/loventure/courseService/LoventureApplication.java
@@ -0,0 +1,13 @@
+package PitterPatter.loventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoventureApplication.class, args);
+	}
+
+}
---
+++ course/src/test/java/PitterPatter/loventure/LoventureApplicationTests.java
@@ -0,0 +1,13 @@
+package PitterPatter.loventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoventureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
