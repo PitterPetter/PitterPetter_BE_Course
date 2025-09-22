610a45fe16a57486b46efd7c88fa3ec39f8f7e4b
Ohjackson <wogus3673@naver.com>
2025-09-22 22:00:58 +0900

    feat: poi save api - pit 172

--- src/main/java/com/example/poi/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.poi.repository;
-
-import com.example.poi.domain.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
